51728a6badfe09e527534effd5a94fe5dec1859c
aliihsand <121578475+aliihsand@users.noreply.github.com>
2025-01-29 13:26:50 +0300

    Proje dosyası ekle.

---
+++ eticaret_uygula/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eticaret_uygula.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        
+        public string ProductPicture { get; set; }
+        public int ProductPrice { get; set; }
+        public int CategoryId { get; set; }
+        virtual public Category Category {  get; set; }
+        [NotMapped]
+        public IFormFile ImageUpload { get; set; }
+    }
+}
